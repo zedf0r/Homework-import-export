67c1a00fe6819e79a5026523afc2d0b361f43994
Leonid <zemlyanykh@bk.ru>
2024-10-21 13:16:31 +0500

    Выполнение заданий

---
+++ game.js
@@ -0,0 +1,15 @@
+import defaultCharacter from './domain.js'
+export default class Game {
+    start() {
+      console.log('game started');
+    }
+  }
+  
+  export class GameSavingData {
+  }
+  
+  export function readGameSaving() {
+  }
+  
+  export function writeGameSaving() {
+  }
